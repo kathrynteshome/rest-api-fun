92c73275fbd62ce896b46d537d49410369413a02
Kathryn Teshome <kathrynteshome@gmail.com>
2023-10-23 17:25:00 -0400

    initial commit

---
+++ src/test/java/com/kathrynteshome/restapifun/RestApiFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kathrynteshome.restapifun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
